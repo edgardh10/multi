399eb93ef805413020be153a25cf95f7ee65c72c
Mario Lozano <medgardh10@gmail.com>
2017-06-20 07:31:12 -0500

    Primer commit

---
+++ index.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="es-PE">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Multitel Servicios de Internet de alta velocidad</title>
+	<meta name="viewport" content="initial-scale=1, maximum-scale=1">	
+	<link href="http://fonts.googleapis.com/css?family=Oswald:300,400,700" rel="stylesheet" type="text/css">
+	<link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,300,400italic,400,600italic,600,700italic,700,800italic,800" rel="stylesheet" type="text/css">
+	<link href='http://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
+	<!--[if lt IE 9]>
+		<script src="assets/documentation/assets/js/html5.js"></script>
+	<![endif]-->
+
+	<!-- LayerSlider stylesheet -->
+	<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/tether/css/tether.min.css">
+	<link rel="stylesheet" href="assets/layerslider/css/layerslider.css" type="text/css">
+	<link rel="stylesheet" href="assets/css/megasystemas.css" type="text/css">
+</head>
+<body class="nobg">
+
+	<!-- LayerSlider markup should be placed after the <body> opening tag-->
+	
+	<?php include('incs/slide.php'); ?>
+	
+	<div class="clearfix"></div>
+	<?php include('incs/menu.php'); ?>
+	<div class="container">
+		
+	</div>
+	
+	<script type="text/javascript">
+		window.Tether = {};
+	</script>
+	<!-- Here comes your other site data -->	
+
+	<!-- External libraries: jQuery & GreenSock -->
+	<script src="assets/layerslider/js/jquery.js" type="text/javascript"></script>
+	<script src="assets/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+	<script type="text/javascript" src="assets/tether/js/tether.min.js"></script>
+	<script src="assets/layerslider/js/greensock.js" type="text/javascript"></script>
+	<!-- LayerSlider script files -->
+	<script src="assets/layerslider/js/layerslider.transitions.js" type="text/javascript"></script>
+	<script src="assets/layerslider/js/layerslider.kreaturamedia.jquery.js" type="text/javascript"></script>
+
+	<!-- Initializing the slider -->
+	<script>
+		$("#layerslider").layerSlider({
+			pauseOnHover: false,
+			skinsPath: 'assets/layerslider/skins/',
+			showCircleTimer: true,
+			navButtons: true,
+			thumbnailNavigation: 'hover'
+		});
+
+		
+
+	</script>
+</body>
+</html>
